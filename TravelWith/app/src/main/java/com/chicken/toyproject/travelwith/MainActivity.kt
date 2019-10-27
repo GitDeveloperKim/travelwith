9814744795d32fb38a73441194f7cd7faa6bcc8a
김영석 <kim-youngseok@gim-yeongseog-ui-MacBook-Pro.local>
2019-10-27 23:47:59 +0900

    android project upload
    코린이 : android project upload

---
+++ TravelWith/app/src/main/java/com/chicken/toyproject/travelwith/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chicken.toyproject.travelwith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
